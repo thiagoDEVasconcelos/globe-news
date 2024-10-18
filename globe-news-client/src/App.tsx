50769e819defbfb608c91302e2521911807b5bb9
Ryan Castelli <ryancastelli19@gmail.com>
2024-10-17 23:04:09 -0300

    Instalação do tailwind

--- globe-news-client/src/App.tsx
+++ globe-news-client/src/App.tsx
@@ -2,6 +2,7 @@ import Header from "./components/Header/header";
 import PostForm from "./components/Form/PostForm";
 import Footer from "./components/Footer/footer";
 import TopNews from "./components/TopNews/topNews";
+import "./index.css"
 
 function App() {
     
