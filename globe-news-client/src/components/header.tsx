9940fe930a37aca390a612a615a7428845276081
Ryan Castelli <ryancastelli19@gmail.com>
2024-10-18 22:02:15 -0300

    style: adjust responsiveness

--- globe-news-client/src/components/header.tsx
+++ globe-news-client/src/components/header.tsx
@@ -4,9 +4,14 @@ import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
 
 const Header = () => {
   return (
-    <header className="bg-white fixed w-full z-50 flex flex-row justify-around py-3 ">
-      <Menu />
-        <ul className="flex flex-row space-x-6 pt-2">
+    <header className="bg-white fixed w-full z-50 flex flex-wrap items-center justify-between px-4 py-3">
+      {/* Menu */}
+      <div className="">
+        <Menu />
+      </div>
+
+      <nav className="hidden sm:flex flex-grow justify-center">
+        <ul className="flex flex-row space-x-4 sm:space-x-6 pt-2">
           <li className="group relative">
             <a href="#">Futebol</a>
             <span className="absolute inset-x-0 bottom-2 h-[2px] bg-black transition-transform duration-300 scale-x-0 group-hover:scale-x-100 origin-center"></span>
@@ -28,14 +33,16 @@ const Header = () => {
             <span className="absolute inset-x-0 bottom-2 h-[2px] bg-black transition-transform duration-300 scale-x-0 group-hover:scale-x-100 origin-center"></span>
           </li>
         </ul>
-        <Box className="flex flex-row">
-          <NotificationsNoneIcon fontSize="large"/>
-          <Stack>
-            <a href="#">
-              <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
-            </a>
-          </Stack>
-        </Box>
+      </nav>
+
+      <Box className="flex items-center space-x-4">
+        <NotificationsNoneIcon fontSize="large" />
+        <Stack>
+          <a href="#">
+            <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+          </a>
+        </Stack>
+      </Box>
     </header>
   );
 };
