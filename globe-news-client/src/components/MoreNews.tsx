025360bfe679d98c65fc93114857f93c7eb67b7f
Ryan Castelli <ryancastelli19@gmail.com>
2024-11-30 18:39:33 -0300

    Layout de mais noticias

---
+++ globe-news-client/src/components/MoreNews.tsx
@@ -0,0 +1,154 @@
+const MoreNews = () => {
+    return (
+      <div className="flex flex-wrap justify-center gap-8 mt-24">
+        <section className="flex flex-col w-[300px] font-bold">
+          <h3 className="mb-6 text-economia text-3xl">Economia</h3>
+          <ul>
+            <li className="flex flex-col items-center mb-6">
+              <a href="#" className="flex flex-row items-center hover:text-economia">
+                <img
+                  src="https://i.pinimg.com/236x/84/af/3c/84af3c67c2824d27b7635dfc06e9674c.jpg"
+                  alt=""
+                  className="w-[175px] h-[150px] object-cover mb-2 rounded-lg"
+                />
+                <h4 className="text-center">
+                  Bitcoin deve “flertar” com a marca de US$ 100 mil a partir do final do ano, prevê Jack Mallers
+                </h4>
+              </a>
+            </li>
+            <li className="flex flex-col items-center mb-6">
+              <a href="#" className="flex flex-row items-center hover:text-economia">
+                <img
+                  src="https://i.pinimg.com/236x/84/af/3c/84af3c67c2824d27b7635dfc06e9674c.jpg"
+                  alt=""
+                  className="w-[175px] h-[150px] object-cover mb-2 rounded-lg"
+                />
+                <h4 className="text-center">
+                  Bitcoin deve “flertar” com a marca de US$ 100 mil a partir do final do ano, prevê Jack Mallers
+                </h4>
+              </a>
+            </li>
+          </ul>
+        </section>
+  
+        <section className="flex flex-col w-[300px] font-bold">
+          <h3 className="mb-6 text-esporte text-3xl">Esporte</h3>
+          <ul>
+            <li className="flex flex-col items-center mb-6">
+              <a href="#" className="flex flex-row items-center hover:text-esporte">
+                <img
+                  src="https://i.pinimg.com/236x/d1/18/0b/d1180bb96f05a6b08316d62b622fbdcb.jpg"
+                  alt=""
+                  className="w-[175px] h-[150px] object-cover mb-2 rounded-lg"
+                />
+                <h4 className="text-center">Rodri acaba de ganhar a bola de ouro!</h4>
+              </a>
+            </li>
+            <li className="flex flex-col items-center mb-6">
+              <a href="#" className="flex flex-row items-center hover:text-esporte">
+                <img
+                  src="https://i.pinimg.com/236x/d1/18/0b/d1180bb96f05a6b08316d62b622fbdcb.jpg"
+                  alt=""
+                  className="w-[175px] h-[150px] object-cover mb-2 rounded-lg"
+                />
+                <h4 className="text-center">Rodri acaba de ganhar a bola de ouro!</h4>
+              </a>
+            </li>
+          </ul>
+        </section>
+  
+        <section className="flex flex-col w-[300px] font-bold">
+          <h3 className="mb-6 text-politica text-3xl">Política</h3>
+          <ul>
+            <li className="flex flex-col items-center mb-6">
+              <a href="#" className="flex flex-row items-center hover:text-politica">
+                <img
+                  src="https://i.pinimg.com/236x/bc/ff/91/bcff911417b06da273404f4ee74d2fff.jpg"
+                  alt=""
+                  className="w-[175px] h-[150px] object-cover mb-2 rounded-lg"
+                />
+                <h4 className="text-center">
+                  Trump ameaça Brics com tarifas caso sigam com planos de desdolarização
+                </h4>
+              </a>
+            </li>
+            <li className="flex flex-col items-center mb-6">
+              <a href="#" className="flex flex-row items-center hover:text-politica">
+                <img
+                  src="https://i.pinimg.com/236x/bc/ff/91/bcff911417b06da273404f4ee74d2fff.jpg"
+                  alt=""
+                  className="w-[175px] h-[150px] object-cover mb-2 rounded-lg"
+                />
+                <h4 className="text-center">
+                  Trump ameaça Brics com tarifas caso sigam com planos de desdolarização
+                </h4>
+              </a>
+            </li>
+          </ul>
+        </section>
+  
+        <section className="flex flex-col w-[300px] font-bold">
+          <h3 className="mb-6 text-gastronomia text-3xl">Gastronomia</h3>
+          <ul>
+            <li className="flex flex-col items-center mb-6">
+              <a href="#" className="flex flex-row items-center hover:text-gastronomia">
+                <img
+                  src="https://i.pinimg.com/736x/01/ec/65/01ec6539c9ed12beb11b142a0ae1b802.jpg"
+                  alt=""
+                  className="w-[175px] h-[150px] object-cover mb-2 rounded-lg"
+                />
+                <h4 className="text-center">
+                  Natal 2024: onde encomendar ceias completas em São Paulo
+                </h4>
+              </a>
+            </li>
+            <li className="flex flex-col items-center mb-6">
+              <a href="#" className="flex flex-row items-center hover:text-gastronomia">
+                <img
+                  src="https://i.pinimg.com/736x/01/ec/65/01ec6539c9ed12beb11b142a0ae1b802.jpg"
+                  alt=""
+                  className="w-[175px] h-[150px] object-cover mb-2 rounded-lg"
+                />
+                <h4 className="text-center">
+                  Natal 2024: onde encomendar ceias completas em São Paulo
+                </h4>
+              </a>
+            </li>
+          </ul>
+        </section>
+  
+        <section className="flex flex-col w-[300px] font-bold">
+          <h3 className="mb-6 text-fofoca text-3xl">Fofocas</h3>
+          <ul>
+            <li className="flex flex-col items-center mb-6">
+              <a href="#" className="flex flex-row items-center hover:text-fofoca">
+                <img
+                  src="https://i.pinimg.com/474x/c0/bf/75/c0bf7532634546dd0b5179ac90870626.jpg"
+                  alt=""
+                  className="w-[175px] h-[150px] object-cover mb-2 rounded-lg"
+                />
+                <h4 className="text-center">
+                  Davi Brito mostra viagem luxuosa a Dubai: “Não tenho palavras”
+                </h4>
+              </a>
+            </li>
+            <li className="flex flex-col items-center mb-6">
+              <a href="#" className="flex flex-row items-center hover:text-fofoca">
+                <img
+                  src="https://i.pinimg.com/474x/c0/bf/75/c0bf7532634546dd0b5179ac90870626.jpg"
+                  alt=""
+                  className="w-[175px] h-[150px] object-cover mb-2 rounded-lg"
+                />
+                <h4 className="text-center">
+                  Davi Brito mostra viagem luxuosa a Dubai: “Não tenho palavras”
+                </h4>
+              </a>
+            </li>
+          </ul>
+        </section>
+      </div>
+    );
+  };
+  
+  export default MoreNews;
+  
