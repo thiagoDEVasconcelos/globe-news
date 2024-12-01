be21a0ef6690c851d5346416e2b6ba03dfa741ba
Ryan Castelli <ryancastelli19@gmail.com>
2024-11-30 21:04:05 -0300

    Melhorando o layout de Top News

--- globe-news-client/src/components/TopNew.tsx
+++ globe-news-client/src/components/TopNew.tsx
@@ -2,32 +2,54 @@ import PoliticaImg from "../assets/halandsiara.jpg";
 
 const TopNew = () => {
   return (
-    <div className="flex justify-center items-center h-screen mt-28">
-  <div className="grid grid-cols-2 grid-rows-2 gap-4 ">
-    <img 
-      src={PoliticaImg} 
-      alt="" 
-      className="w-[502px] h-[502px] object-cover"
-    />
-    <img 
-      src="https://conteudo.imguol.com.br/c/esporte/00/2024/10/22/vini-jr-comemora-gol-em-real-madrid-x-borussia-dortmund-na-champions-league-1729634369290_v2_1x1.jpg" 
-      alt="" 
-      className="w-[502px] h-[502px] object-cover"
-    />
-    <img 
-      src="https://fly.metroimg.com/upload/q_85,w_700/https://uploads.metroimg.com/wp-content/uploads/2024/05/23104409/GettyImages-2153616294.jpg" 
-      alt="" 
-      className="w-[502px] h-[502px] object-cover"
-    />
-    <img 
-      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1cp9UtNUZ7OvASa_h7Pxd26LPE0Vm99gzYQ&s" 
-      alt="" 
-      className="w-[502px] h-[502px] object-cover"
-    />
-  </div>
-</div>
-
-
+    <div className="flex justify-center items-center h-screen mt-36">
+      <div className="grid grid-cols-2 grid-rows-2 gap-4 ">
+        <a href="" className="relative w-[502px] h-[502px] hover:text-red-700 text-white">
+          <img
+            src={PoliticaImg}
+            alt=""
+            className="w-full h-full object-cover"
+          />
+          <span className="absolute inset-0 bg-black bg-opacity-20"></span>
+          <h1 className="absolute inset-0 flex flex-col justify-end  text-2xl font-extrabold p-4">
+            Haaland é o mais novo reforço do Ceará para 2025!
+          </h1>
+        </a>
+        <a href="" className="relative w-[502px] h-[502px] hover:text-red-700 text-white">
+          <img
+            src="https://conteudo.imguol.com.br/c/esporte/00/2024/10/22/vini-jr-comemora-gol-em-real-madrid-x-borussia-dortmund-na-champions-league-1729634369290_v2_1x1.jpg"
+            alt=""
+            className="w-full h-full object-cover"
+          />
+          <span className="absolute inset-0 bg-black bg-opacity-20"></span>
+          <h1 className="absolute inset-0 flex flex-col justify-end  text-2xl font-extrabold p-4">
+            Vini Jr Faz Hat-Trick aos 45 do segundo Tempo!
+          </h1>
+        </a>
+        <a href="" className="relative w-[502px] h-[502px] hover:text-red-700 text-white">
+          <img
+            src="https://fly.metroimg.com/upload/q_85,w_700/https://uploads.metroimg.com/wp-content/uploads/2024/05/23104409/GettyImages-2153616294.jpg"
+            alt=""
+            className="w-full h-full object-cover"
+          />
+          <span className="absolute inset-0 bg-black bg-opacity-20"></span>
+          <h1 className="absolute inset-0 flex flex-col justify-end  text-2xl font-extrabold p-4">
+            URGENTE! Milei fala que Messi vai pegar o beco!
+          </h1>
+        </a>
+        <a href="" className="relative w-[502px] h-[502px] hover:text-red-700 text-white">
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1cp9UtNUZ7OvASa_h7Pxd26LPE0Vm99gzYQ&s"
+            alt=""
+            className="w-full h-full object-cover"
+          />
+          <span className="absolute inset-0 bg-black bg-opacity-20"></span>
+          <h1 className="absolute inset-0 flex flex-col justify-end  text-2xl font-extrabold p-4">
+            Imagem completamente aleátória jutanmente com simulação de notícia!
+          </h1>
+        </a>
+      </div>
+    </div>
   );
 };
 
