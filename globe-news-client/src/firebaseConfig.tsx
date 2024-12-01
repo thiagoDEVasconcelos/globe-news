195b840fa5096affdc4036c2473a1245497c12a3
Ryan Castelli <ryancastelli19@gmail.com>
2024-11-30 23:18:00 -0300

    Config do Firebase

---
+++ globe-news-client/src/firebaseConfig.tsx
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyBZujl-siAsR9Dtyvksr40cTeIVg9xF_S8",
+  authDomain: "globe-news-ab4bf.firebaseapp.com",
+  projectId: "globe-news-ab4bf",
+  storageBucket: "globe-news-ab4bf.firebasestorage.app",
+  messagingSenderId: "36487747118",
+  appId: "1:36487747118:web:48055ec9a523e0e40bf1bf",
+  measurementId: "G-GLZ58WP7Q9"
+};
+
+export const app = initializeApp(firebaseConfig);
